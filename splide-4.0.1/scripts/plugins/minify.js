d051b981a36a2cf350f8bc34e0b076293e0b4376
EdiJunior88 <er.moura88@yahoo.com.br>
2022-04-28 22:17:29 -0300

    Atualizções importantes no HTML e CSS - Criação da section Projetos - Adição do plugin Splide JS - Criação de um Carousel (galeria de imagens) - Pequenas atualizações no CSS do Carousel - Adição de imagens para os projetos no Carousel

---
+++ splide-4.0.1/scripts/plugins/minify.js
@@ -0,0 +1,36 @@
+const uglify = require( 'uglify-js' );
+const plugin = require( 'rollup-plugin-dts' );
+
+const DEFAULTS = {
+  minify: {
+	  sourceMap: true,
+    output: {
+      comments: /^!/,
+    },
+    toplevel: true,
+    mangle: {
+      properties: {
+        regex: /^_/,
+      },
+    },
+  },
+};
+
+function minify( pluginOptions = {} ) {
+  pluginOptions = { ...DEFAULTS, ...pluginOptions };
+
+  return {
+    name: 'minify',
+    renderChunk( code ) {
+      const result = uglify.minify( code, pluginOptions.minify );
+
+      if ( result.error ) {
+        throw new Error( result.error );
+      }
+
+      return result;
+    },
+  }
+}
+
+exports.minify = minify;
