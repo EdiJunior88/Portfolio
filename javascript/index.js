9f8f977a8e376632e208cc09b1ebbdb5dbeee488
EdiJunior88 <er.moura88@yahoo.com.br>
2022-05-03 11:40:12 -0300

    Atualização no HTML, CSS e JAVASCRIPT - Separação das Tags com espaçamento - Ajustes finos no CSS (design da página) - Criação de uma automatização da minha idade através do JavaScript

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+/* Cálculo de idade atual */
+const aniversario = new Date("March 26, 1988");
+let hoje = new Date();
+
+let diferenca =  hoje - aniversario;
+
+let idade = new Date(diferenca).getFullYear() - 1970;
+
+let span = document.getElementById('aniversario');
+span.innerHTML = idade;
