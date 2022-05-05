4df2f9bd6ba49f95bf19fef3972759ff1ff8bcfd
EdiJunior88 <er.moura88@yahoo.com.br>
2022-05-04 21:28:07 -0300

    Alterações no HTML, CSS e JAVASCRIPT - Ajustes nas tags - Ajustes no tamanho do background, cor do texto e tamanho do carousel - Criação de uma função para mostrar a idade

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,14 @@
-/* Cálculo de idade atual */
-const aniversario = new Date("March 26, 1988");
-let hoje = new Date();
+/* Função Cálculo de idade atual */
+function dataAniversario () {
+  const aniversario = new Date("March 26, 1988");
+  let hoje = new Date();
+  
+  let diferenca =  hoje - aniversario;
+  
+  let idade = new Date(diferenca).getFullYear() - 1970;
+  
+  let span = document.getElementById('aniversario');
+  span.innerHTML = idade;
+}
 
-let diferenca =  hoje - aniversario;
-
-let idade = new Date(diferenca).getFullYear() - 1970;
-
-let span = document.getElementById('aniversario');
-span.innerHTML = idade;
+dataAniversario();
