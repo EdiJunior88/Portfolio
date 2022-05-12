5313e2b75979ba0dc859ab418cc59bbd185ed49c
EdiJunior88 <er.moura88@yahoo.com.br>
2022-05-11 23:45:23 -0300

    Atualizações no HTML, CSS e JAVASCRIPT - Criação do menu hamburguer no HTML - Estilização do menu superior no CSS - Criação da função abrir e fechar o menu hamburguer - Criação do Media Querie para celular (max-width: 490px)

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,24 @@
 /* Função Cálculo de idade atual */
-function dataAniversario () {
-  const aniversario = new Date("March 26, 1988");
+function dataAniversario() {
+  const aniversario = new Date('March 26, 1988');
   let hoje = new Date();
-  
-  let diferenca =  hoje - aniversario;
-  
+
+  let diferenca = hoje - aniversario;
+
   let idade = new Date(diferenca).getFullYear() - 1970;
-  
+
   let span = document.getElementById('aniversario');
   span.innerHTML = idade;
 }
 
 dataAniversario();
+
+/* Função menu Hamburguer */
+const botaoMobile = document.getElementById('botao-menu');
+
+function abrirMenu() {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('ativo');
+}
+
+botaoMobile.addEventListener('click', abrirMenu);
